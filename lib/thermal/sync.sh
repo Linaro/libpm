cca517c6c13471bbfbbb98bc044d1d5e33c289e3
Daniel Lezcano <daniel.lezcano@linaro.org>
2024-12-03 23:43:11 +0100

    Improve script to synchronize the upstream library
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>

--- lib/thermal/sync.sh
+++ lib/thermal/sync.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
 # set -ax
 GIT_LINUX_PATH=".linux"
+GIT_LINUX_LIB_THERMAL="tools/lib/thermal"
 GIT_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+GIT_REFERENCE=""
+
+while getopts ":r:h" o; do
+    case "${o}" in
+        r)
+            GIT_REFERENCE="--reference=${OPTARG}"
+            ;;
+	h)
+	    echo "$0 [-r <git reference>]"
+	    exit 0
+	    ;;
+        *)
+	    echo "Unknown parameter"
+	    exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
 
 if [ ! -d $GIT_LINUX_PATH ]; then
     echo " * Cloning $GIT_REPO"
-    git clone --depth 1 --sparse $GIT_REPO $GIT_LINUX_PATH
+    git clone -j 8 $GIT_REFERENCE $GIT_REPO $GIT_LINUX_PATH
 fi
 
-pushd $GIT_LINUX_PATH > /dev/null
+pushd $GIT_LINUX_PATH || exit 1
 
 # Update the official Linus' tree
 echo " * Updating $GIT_REPO"
 git pull
 
-GIT_LIB_THERMAL="tools/lib/thermal"
-GIT_THERMAL_LIB="tools/thermal/lib"
-GIT_SPARSE_SET="$GIT_LIB_THERMAL $GIT_THERMAL_LIB"
-
-echo " * Sparse checkout $GIT_SPARSE_SET"
-git sparse-checkout set $GIT_SPARSE_SET
-
 popd > /dev/null
+
+ln -sf $PWD/.linux/include/uapi/linux include
 
 INCLUDE_PATH=src/include
 LIB_PATH=src/lib
-THERMAL_MANAGER_PATH=src/thermal-manager
 
 declare -A TARGETS
 
-TARGETS["include"]="$GIT_LINUX_PATH/$GIT_LIB_THERMAL/include/*.[ch]"
-TARGETS["src"]="$GIT_LINUX_PATH/$GIT_LIB_THERMAL/*.[ch] $GIT_LINUX_PATH/$GIT_LIB_THERMAL/Makefile"
+TARGETS["include"]="$GIT_LINUX_PATH/$GIT_LINUX_LIB_THERMAL/include/*.[ch]"
+TARGETS["src"]="$GIT_LINUX_PATH/$GIT_LINUX_LIB_THERMAL/*.[ch]"
 
 echo " * Updating source files"
 for TARGET in ${!TARGETS[@]}; do
@@ -45,7 +59,10 @@ done
 
 echo " * Patching source files"
 for PATCH in $(cat patches/patches.list); do
-    patch -p1 < patches/$PATCH
+    patch -p1 -N -r - < patches/$PATCH
 done
+
+echo " * Installing kernel headers in 'include'"
+make -C .linux ARCH=arm64 INSTALL_HDR_PATH=$PWD headers_install
 
 echo "Done"
